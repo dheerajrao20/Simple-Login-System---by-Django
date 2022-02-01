ab6ad15583a8e44cf4f9909046cb1673fdf2e3c8
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-02-01 13:38:44 +0530

    Update info.py

--- loginpage/info.py
+++ loginpage/info.py
@@ -1,5 +1,8 @@
+# update the page according to your credentials to use the source code in your projects
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'boywithcodes@gmail.com'
-EMAIL_HOST_PASSWORD = 'iamRAJ20@'
+EMAIL_HOST_USER = 'yourgmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'enter your email password here'
 EMAIL_PORT = 587
+
