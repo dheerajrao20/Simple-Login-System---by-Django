74f19f5117936292e4cdee4eb2f71e384fb2c819
DHEERAJ KUMAR <89007097+dheerajrao20@users.noreply.github.com>
2022-02-02 17:57:32 +0530

    Update settings.py

--- loginpage/settings.py
+++ loginpage/settings.py
@@ -27,7 +27,7 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'use your security key here'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
